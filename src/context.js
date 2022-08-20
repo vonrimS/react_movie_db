52ac95c6953ae59d97fb8b9dc3f2556a336cfa11
vonrimS <mr.mytab@gmail.com>
2022-08-20 19:28:01 +0200

    initial fetch

--- src/context.js
+++ src/context.js
@@ -1,10 +1,43 @@
 import React, { useState, useContext, useEffect } from 'react';
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+console.log(API_ENDPOINT);
+console.log(process.env.REACT_APP_MOVIE_API_KEY);
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState({ show: false, msg: '' });
+    const [movies, setMovies] = useState([]);
+    const [query, setQuery] = useState('hobbit');
+
+    const fetchMovies = async (url) => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            if (data.response === 'True') {
+                setMovies(data.Search);
+                setError({ show: false, msg: '' });
+            }
+            else {
+                setError({ show: true, msg: data.Error });
+            }
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchMovies(`${API_ENDPOINT}&s=${query}`);
+    }, []);
+
+    return <AppContext.Provider
+        value={{ isLoading, error, movies, query, setQuery }}>
+        {children}
+    </AppContext.Provider>;
 };
 // make sure use
 export const useGlobalContext = () => {
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
