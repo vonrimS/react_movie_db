4752c142cde7324990a87bc05a620613a770f808
vonrimS <mr.mytab@gmail.com>
2022-08-20 22:22:43 +0200

    useFetch refactoring

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,29 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { API_ENDPOINT } from './context';
+import useFetch from './useFetch';
 
 const SingleMovie = () => {
     const { id } = useParams();
-    const [movie, setMovie] = useState({});
-    const [isLoading, setLoading] = useState(true);
-    const [error, setError] = useState({ show: false, msg: '' });
+    const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
 
-    const fetchMovie = async (url) => {
-        const response = await fetch(url);
-        const data = await response.json();
-        // console.log(data);
-        if (data.Response === 'False') {
-            setError({ show: true, msg: data.Error });
-            setLoading(false);
-        } else {
-            setMovie(data);
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchMovie(`${API_ENDPOINT}&i=${id}`);
-    }, [id]);
 
     if (isLoading) {
         return <div className="loading"></div>;
